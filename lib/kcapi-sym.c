3dbce6193a948f7face57f98dcf2a2527a55db92
Stephan Mueller <smueller@chronox.de>
2016-12-07 15:12:42 +0100

    add AIO fallback code
    In case AIO is not present on the system, but the AIO API is used,
    fall back to the non-AIO interface transparently.

--- lib/kcapi-sym.c
+++ lib/kcapi-sym.c
@@ -62,14 +62,51 @@ int32_t kcapi_cipher_encrypt(struct kcapi_handle *handle,
 					 ALG_OP_ENCRYPT);
 }
 
+/*
+ * Fallback function if AIO is not present, but caller requested AIO operation.
+ */
+static int32_t
+_kcapi_cipher_encrypt_aio_helper(struct kcapi_handle *handle,
+				 struct iovec *iniov, struct iovec *outiov,
+				 uint32_t iovlen, const uint8_t *iv)
+{
+	int32_t ret = 0;
+	uint32_t i;
+
+	/* every IOVEC for AIO is an independent cipher invocation */
+	for (i = 0; i < iovlen; i++) {
+		int32_t rc = kcapi_cipher_stream_init_enc(handle, iv, &iniov[i],
+							  1);
+
+		if (rc < 0)
+			return rc;
+
+		rc = kcapi_cipher_stream_op(handle, &outiov[i], 1);
+		if (rc < 0)
+			return rc;
+
+		ret += rc;
+	}
+
+	return ret;
+}
+
 DSO_PUBLIC
 int32_t kcapi_cipher_encrypt_aio(struct kcapi_handle *handle,
 				 struct iovec *iniov, struct iovec *outiov,
 				 uint32_t iovlen, const uint8_t *iv, int access)
 {
+	int32_t ret;
+
 	handle->cipher.iv = iv;
-	return _kcapi_cipher_crypt_aio(handle, iniov, outiov, iovlen, access,
-				       ALG_OP_ENCRYPT);
+	ret = _kcapi_cipher_crypt_aio(handle, iniov, outiov, iovlen, access,
+				      ALG_OP_ENCRYPT);
+
+	if (ret == -EOPNOTSUPP)
+		return _kcapi_cipher_encrypt_aio_helper(handle, iniov, outiov,
+							iovlen, iv);
+
+	return ret;
 }
 
 DSO_PUBLIC
@@ -98,14 +135,51 @@ int32_t kcapi_cipher_decrypt(struct kcapi_handle *handle,
 					 ALG_OP_DECRYPT);
 }
 
+/*
+ * Fallback function if AIO is not present, but caller requested AIO operation.
+ */
+static int32_t
+_kcapi_cipher_decrypt_aio_helper(struct kcapi_handle *handle,
+				 struct iovec *iniov, struct iovec *outiov,
+				 uint32_t iovlen, const uint8_t *iv)
+{
+	int32_t ret = 0;
+	uint32_t i;
+
+	/* every IOVEC for AIO is an independent cipher invocation */
+	for (i = 0; i < iovlen; i++) {
+		int32_t rc = kcapi_cipher_stream_init_dec(handle, iv, &iniov[i],
+							  1);
+
+		if (rc < 0)
+			return rc;
+
+		rc = kcapi_cipher_stream_op(handle, &outiov[i], 1);
+		if (rc < 0)
+			return rc;
+
+		ret += rc;
+	}
+
+	return ret;
+}
+
 DSO_PUBLIC
 int32_t kcapi_cipher_decrypt_aio(struct kcapi_handle *handle,
 				 struct iovec *iniov, struct iovec *outiov,
 				 uint32_t iovlen, const uint8_t *iv, int access)
 {
+	int32_t ret;
+
 	handle->cipher.iv = iv;
-	return _kcapi_cipher_crypt_aio(handle, iniov, outiov, iovlen, access,
-				       ALG_OP_DECRYPT);
+	ret = _kcapi_cipher_crypt_aio(handle, iniov, outiov, iovlen, access,
+				      ALG_OP_DECRYPT);
+
+	if (ret == -EOPNOTSUPP)
+		return _kcapi_cipher_decrypt_aio_helper(handle, iniov, outiov,
+							iovlen, iv);
+
+	return ret;
 }
 
 DSO_PUBLIC
