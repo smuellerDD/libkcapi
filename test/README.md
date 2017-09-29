b318b9ec9a6c2cf29de3b8f8db82d0118f1ef3d2
Stephan Mueller <smueller@chronox.de>
2017-09-29 22:54:55 +0200

    Add fuzzing test automation
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- test/README.md
+++ test/README.md
@@ -38,13 +38,13 @@ as listed in the following:
   test scripts. It is not intended to be called directly.
 
 All tests are collectively invoked with the `test-invocation.sh` script.
+In addition, the following tests are not integrated into the general
+test run:
+
+* `kcapi-fuzz-test.sh`: Execute various fuzzing tests.
 
 Bi-arch tests
 -------------
 
-On bi-arch systems, the Makefile.m32 can be used to compile the 32 bit
-version of the test application.
-
 To test both word sizes simultaneously, use test-invocation.sh which
-compiles and executes the test cases. Again, the script shall
-return with a return code of 0.
+compiles and executes the test cases.
---
+++ test/kcapi-fuzz-test.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# Copyright (C) 2017, Stephan Mueller <smueller@chronox.de>
+#
+# License: see LICENSE file in root directory
+#
+# THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ALL OF
+# WHICH ARE HEREBY DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE, EVEN IF NOT ADVISED OF THE POSSIBILITY OF SUCH
+# DAMAGE.
+#
+
+. libtest.sh
+
+KCAPI="${APPDIR}/kcapi"
+find_platform $KCAPI
+
+ROUNDS=100
+
+SYMCIPHER="cbc(aes)"
+AEADCIPHER="authenc(hmac(sha256),cbc(aes)) gcm(aes)"
+
+checkret()
+{
+	local ret=$1
+	shift
+
+	if [ $ret -eq 0 ]
+	then
+		echo_pass "Fuzz test $@"
+	else
+		echo_fail "Fuzz test $@"
+	fi
+}
+
+for i in $SYMCIPHER
+do
+	$KCAPI -h -x 1 -c "$i" -d $ROUNDS
+	checkret $? "$i synchronous"
+done
+
+if $(check_min_kernelver 4 1); then
+	for i in $AEADCIPHER
+	do
+		$KCAPI -h -x 2 -c "$i" -d $ROUNDS
+		checkret $? "$i synchronous"
+	done
+else
+	echo_deact "AEAD fuzz tests deactivated"
+fi
+
+echo "==================================================================="
+echo "Number of failures: $failures"
