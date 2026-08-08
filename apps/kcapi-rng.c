df40ea48ecbf1afc6d3efa5494673fa73bd40826
Stephan Mueller <smueller@chronox.de>
2026-08-08 17:58:57 +0200

    bump copyright year
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- apps/kcapi-rng.c
+++ apps/kcapi-rng.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 - 2024, Stephan Mueller <smueller@chronox.de>
+ * Copyright (C) 2017 - 2026, Stephan Mueller <smueller@chronox.de>
  *
  * License: see COPYING file in root directory
  *
